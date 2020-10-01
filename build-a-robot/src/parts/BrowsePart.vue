fcedbcb9515c3e8030b0479346336fcc7396a1fc
jacobzouzou <jacob.zouzou@gmail.com>
2020-10-01 12:01:11 +0200

    Fix action getParts from module issue

--- build-a-robot/src/parts/BrowsePart.vue
+++ build-a-robot/src/parts/BrowsePart.vue
@@ -2,10 +2,10 @@
   <div>
     <h1>Browse Parts</h1>
     <ul class="menu">
-      <li><router-link :to="{name:'BrowseHeads'}">Heads</router-link></li>
-      <li><router-link :to="{name:'BrowseArms'}">Arms</router-link></li>
-      <li><router-link :to="{name:'BrowseTorsos'}">Torsos</router-link></li>
-      <li><router-link :to="{name:'BrowseBases'}">Bases</router-link></li>
+      <li><router-link :to="{ name: 'BrowseHeads' }">Heads</router-link></li>
+      <li><router-link :to="{ name: 'BrowseArms' }">Arms</router-link></li>
+      <li><router-link :to="{ name: 'BrowseTorsos' }">Torsos</router-link></li>
+      <li><router-link :to="{ name: 'BrowseBases' }">Bases</router-link></li>
     </ul>
     <router-view></router-view>
   </div>
@@ -13,24 +13,24 @@
 
 <script>
 export default {
-  name: 'BrowseParts',
+  name: "BrowseParts"
 };
 </script>
 
 <style scoped>
-  h1 {
-    text-align: center;
-  }
-  .menu {
-    display: flex;
-    justify-content: space-around;
-    margin: 0 100px;
-    font-size: 20px;
-  }
-  ul {
-    list-style-type: none;
-  }
-  a {
-    color: palevioletred;
-  }
+h1 {
+  text-align: center;
+}
+.menu {
+  display: flex;
+  justify-content: space-around;
+  margin: 0 100px;
+  font-size: 20px;
+}
+ul {
+  list-style-type: none;
+}
+a {
+  color: palevioletred;
+}
 </style>
